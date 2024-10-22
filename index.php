746091cf480e0ff3b1239f869c7ff54ebdcd64d4
natalia izquierdo dominguez <nizquierdod04@iesvjp.es>
2024-10-22 20:09:55 +0200

    partials modificado

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+
+    require 'utils/utils.php';
+    require 'entities/imagenGaleria.class.php';
+    
+    $arrayImagenes = array();
+    function crearImagenes(){
+        for($i=1; $i<=12; $i++){
+           $imagen = new ImagenGaleria(`{$i}.jpg`,`descrion imagen {$i}`,numAleatorio(),numAleatorio(),numAleatorio());
+            array_unshift($arrayImagenes, $imagen);
+            
+        }
+    }
+    function numAleatorio() : int {
+        return round(0,1500);
+    }
     require 'views/index.view.php';
 ?>
