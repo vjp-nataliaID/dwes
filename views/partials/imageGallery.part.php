48254844d6c8d0681736bb10302243273dac053c
natalia izquierdo dominguez <nizquierdod04@iesvjp.es>
2024-10-23 20:17:43 +0200

    gfhbfgh

--- views/partials/imageGallery.part.php
+++ views/partials/imageGallery.part.php
@@ -1,12 +1,15 @@
-   <?php for ($i = 0; $i < count($arrayImagenes); $i++): ?>
+   <?php
+   shuffle($arrayImagenes);
+   
+   for ($i = 0; $i < count($arrayImagenes); $i++): ?>
        <div class="col-xs-12 col-sm-6 col-md-3">
            <div class="sol">
-               <img class="img-responsive" src="<?php $arrayImagenes[$i]->getUrlPortfolio()?>" alt="<?php $arrayImagenes[$i]->getDescripcion()?>">
+               <img class="img-responsive" src=<?= $arrayImagenes[$i]->getUrlPortfolio()?> alt=<?= $arrayImagenes[$i]->getDescripcion()?>>
                <div class="behind">
                    <div class="head text-center">
                        <ul class="list-inline">
                            <li>
-                               <a class="gallery" href="<?php $arrayImagenes[$i]->getUrlPortfolio()?>" data-toggle="tooltip" data-original-title="Quick View">
+                               <a class="gallery" href="<?= $arrayImagenes[$i]->getUrlPortfolio()?>" data-toggle="tooltip" data-original-title="Quick View">
                                    <i class="fa fa-eye"></i>
                                </a>
                            </li>
@@ -29,9 +32,9 @@
                    </div>
                    <div class="row box-content">
                        <ul class="list-inline text-center">
-                           <li><i class="fa fa-eye"></i><?php $arrayImagenes[$i]->getNumVisualizaciones() ?></li>
-                           <li><i class="fa fa-heart"></i><?php $arrayImagenes[$i]->getNumLikes() ?></li>
-                           <li><i class="fa fa-download"></i><?php $arrayImagenes[$i]->getNumDownloads() ?></li>
+                           <li><i class="fa fa-eye"></i><?= $arrayImagenes[$i]->getNumVisualizaciones() ?></li>
+                           <li><i class="fa fa-heart"></i><?= $arrayImagenes[$i]->getNumLikes() ?></li>
+                           <li><i class="fa fa-download"></i><?= $arrayImagenes[$i]->getNumDownloads() ?></li>
                        </ul>
                    </div>
                </div>
